a90c26b9ef0104fc3e602a767b0a03b5f9577e3a
Kishore <kishore22705@gmail.com>
2025-01-23 20:14:37 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import LoadingScreen from './components/LoadingScreen/LoadingScreen'
+const App = () => {
+  return (
+    <main >
+      <LoadingScreen/>
+      <section className='Main'>
+        hi
+      </section>
+    </main>
+  )
+}
+
+export default App
